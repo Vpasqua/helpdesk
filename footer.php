320f9269eb0f2eaae857b0f4769c766e8827e32c
pasqua435 <83291047+pasqua435@users.noreply.github.com>
2021-09-22 11:03:43 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+	<div class="row">
+		<div class="col-md-4">
+			<img src="images/logo.png"/>
+		</div>
+		<div class="col-md-4">
+			
+DESENVOLVIDO POR <a href="https://github.com/pasqua435/helpdesk/">3º Sgt STT PASQUALOTTO</a>
+		</div>
+		<div class="col-md-4">
+		</div>
+	</div>
+</footer>
