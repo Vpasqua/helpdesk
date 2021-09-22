cfb838defd076aa2921c0a1bcb96dd0230c70fa0
pasqua435 <83291047+pasqua435@users.noreply.github.com>
2021-09-22 11:04:09 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">Index</a>
+			</div>
+<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="acessa.php">Login</a></li>
+					<li><a href="sair.php">Logoff</a></li>  
+				</ul>
+				<form class="navbar-form navbar-left">
+					<div class="form-group">
+					  <input type="text" class="form-control" placeholder="Pesquise aqui">
+					</div>
+					<button type="submit" class="btn btn-default">Procura</button>
+				</form>
+			
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
