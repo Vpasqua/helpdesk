209822d5a565e12f0f66ce37bbceaf6c02466b93
pasqua435 <83291047+pasqua435@users.noreply.github.com>
2021-09-22 11:02:27 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="author" content="pasqua435">
+	<meta http-equiv="content-language" content="pt-br">
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<meta name="reply-to" content="vinicius@artesano.com.br">
+	<link href="css/index.css" rel="stylesheet">
+	<link href="css/estilo.css" rel="stylesheet">
+</head>
+<body>
+	<?php include'header.php'; ?>
+	<?php include'login.php'; ?>
+	<?php include'footer.php'; ?>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	</body>
+<footer>
+</footer>
+</html>
