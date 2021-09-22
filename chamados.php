1d36953af457f22ea0601ba3e7c57c1d55e24bd1
pasqua435 <83291047+pasqua435@users.noreply.github.com>
2021-09-22 11:02:52 -0300

    Create chamados.php

---
+++ chamados.php
@@ -0,0 +1,22 @@
+<body>
+	<form>
+	<div class="container-login">
+		<div class="form-group">
+			<label for="inputUnidade">Unidade</label>
+			<input type="name" class="form-control" id="inputUnidade" placeholder="Unidade Fabril">
+		</div>
+		<div class="form-group">
+			<label for="inputUser">Usuário</label>
+			<input type="name" class="form-control" id="inputUser" placeholder="Usuário">
+		</div>
+		<div class="form-group">
+			<label for="inputMaquina">Máquina com defeito</label>
+			<input type="name" class="form-control" id="inputMaquina" placeholder="Máquina com defeito">
+		</div>
+		<div class="form-group">
+			<input type="name" class="form-control" id="inpuProblema" placeholder="Descrição do problema">
+		</div>
+	<button type="submit" class="btn btn-artesano">Submit</button>
+	</div>
+</form>
+</body>
